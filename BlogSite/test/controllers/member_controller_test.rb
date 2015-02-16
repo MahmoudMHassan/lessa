a88acbb2a17d15da0c6d0410e99b920cf546109f
Rainbow Zephyr <darkfeathers9@gmail.com>
2015-02-16 22:36:51 +0200

    half baked sign up and delete post

---
+++ BlogSite/test/controllers/member_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class MemberControllerTest < ActionController::TestCase
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should get sign_up" do
+    get :sign_up
+    assert_response :success
+  end
+
+  test "should get login" do
+    get :login
+    assert_response :success
+  end
+
+end
