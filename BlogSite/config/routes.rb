ee460ca7185bca8d55daea3b7a6101fde94cc37f
Rainbow Zephyr <darkfeathers9@gmail.com>
2015-02-16 21:51:13 +0200

    Created posts controller

--- BlogSite/config/routes.rb
+++ BlogSite/config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  post 'posts/new'
+
+  get 'posts/edit'
+
+  get 'posts/delete'
+
+  get 'posts/index'
+  
+  delete 'posts/:id' => 'posts#delete'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
